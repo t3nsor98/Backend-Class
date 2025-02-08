eb4375601c027595f8e5d70b01a9502ef0bb99a5
Digbijaya Lenka <digbijayalenka@gmail.com>
2025-02-09 03:31:33 +0530

    added merged snippet

--- project1/index.js
+++ project1/index.js
@@ -10,6 +10,22 @@ app.get("/api/users", (req, res) => {
   return res.json(users);
 });
 
+app.get("/api/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const user = users.find((user) => user.id == id);
+  return res.json(user);
+});
+
+// create new user
+app.post("/api/users", (req, res) => {
+  // TODO: create new user
+  return res.json({ status: "pending" });
+});
+
+// merged
+
+// app.route("/api/users/:id").put((req, res) => {}).delete((req, res) => {}).patch((req, res) => {});
+
 app.get("/users", (req, res) => {
   const html = `
     <ul>
